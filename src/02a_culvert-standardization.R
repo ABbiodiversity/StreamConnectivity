c0d0c2ea666f25797955d434a3477e0b1c20940c
beallen <ballen@ualberta.ca>
2023-05-24 10:08:54 -0600

    Updated how culvert surveys are selected for the hanging culvert model.

--- src/02a_culvert-standardization.R
+++ src/02a_culvert-standardization.R
@@ -1,7 +1,7 @@
 #
 # Title: Standardizing the culvert data
 # Created: March 31st, 2023
-# Last Updated: April 25th, 2023
+# Last Updated: May 18th, 2023
 # Author: Brandon Allen
 # Objectives: Standardize the culvert data from various sources in preparation for modeling
 # Keywords: Notes, Standardization, Extraction from matching, Culvert Predictions
@@ -32,13 +32,15 @@ library(sf)
 woodlands.north <- read.csv("data/base/culvert-surveys/woodlands-north/woodlands-sampling_2019-11-07.csv")
 woodlands.north$SurveyID <- 1:nrow(woodlands.north)
 woodlands.north$SurveyDate <- as.Date("2019-10-01") # Performed fall of 2019
-woodlands.north <- woodlands.north[, c("SurveyID", "SurveyDate", "Lat", "Long", "Fish.Passage.Concern", "Fish.Passage.Concern.Reason")]
-colnames(woodlands.north) <- c("SurveyID", "SurveyDate", "Latitude", "Longitude", "Passability", "PassabilityConcern")
+woodlands.north$Project <- "WoodlandsNorth"
+woodlands.north <- woodlands.north[, c("SurveyID", "SurveyDate", "Project", "Lat", "Long", "Stream.Class", "Fish.Passage.Concern", "Fish.Passage.Concern.Reason")]
+colnames(woodlands.north) <- c("SurveyID", "SurveyDate", "Project", "Latitude", "Longitude", "StreamClass", "Passability", "PassabilityConcern")
 woodlands.north$Passability[woodlands.north$Passability == "NULL"] <- NA
 woodlands.north$PassabilityConcern[woodlands.north$PassabilityConcern == "NULL"] <- NA
 woodlands.north$PassabilityConcern[woodlands.north$PassabilityConcern == "Hanging culvert"] <- "Hanging Culvert"
 woodlands.north <- woodlands.north[!is.na(woodlands.north$Passability), ]
 woodlands.north <- woodlands.north[!duplicated(woodlands.north[, -1]), ] # Remove duplicated calls
+woodlands.north <- woodlands.north[woodlands.north$StreamClass %in% c("Large permanent", "Small permanent"), ]
 
 #
 # National Parks
@@ -46,12 +48,14 @@ woodlands.north <- woodlands.north[!duplicated(woodlands.north[, -1]), ] # Remov
 
 national.parks <- read.csv("data/base/culvert-surveys/national-park/parks-canada-culverts_2021-04-07.csv")
 national.parks$SurveyDate <- as.Date("2008-08-04") # Performed summer of 2008
-national.parks <- national.parks[, c("OBJECTID", "SurveyDate", "ycoord", "xcoord", "Passability", "PassabilityConcern")]
-colnames(national.parks) <- c("SurveyID", "SurveyDate", "Latitude", "Longitude", "Passability", "PassabilityConcern")
+national.parks$StreamClass <- "Small permanent" # Assumed permanent 
+national.parks$Project <- "NationalParks"
+national.parks <- national.parks[, c("OBJECTID", "SurveyDate", "Project", "ycoord", "xcoord", "StreamClass", "Passability", "PassabilityConcern")]
+colnames(national.parks) <- c("SurveyID", "SurveyDate", "Project", "Latitude", "Longitude", "StreamClass", "Passability", "PassabilityConcern")
 national.parks <- national.parks[!duplicated(national.parks[, -1]), ] # Remove duplicated calls
 
 #
-# Watercourse Crossing Program
+# Watercourse Crossing Program 
 # 
 
 wcp <- read_xlsx("data/base/culvert-surveys/watercourse-crossing-program/inspections_2023-03-31.xlsx", sheet = "inspections_2023-03-31")
@@ -59,17 +63,27 @@ wcp <- wcp[wcp$POINT_X > -120, ] # Remove stray locations
 wcp <- wcp[wcp$POINT_Y > 49, ]
 wcp$INSP_DATE <- as.Date(wcp$INSP_DATE) # Keep only inspection dates post 2010
 wcp <- wcp[wcp$INSP_DATE > as.Date("2010-01-01"), ] 
-wcp <- wcp[-grep("ABMI", wcp$REMARKS), ] # Remove the duplicated ABMI surveys
-wcp$Reason[wcp$CV_OUT_TY == "Hanging"] <- "Hanging Culvert" # If we have a culvert outlet type call, define the reason as Hanging
-wcp$Passability <- wcp$FISHPCONC 
-wcp$Passability[wcp$Reason != "NA"] <- "Serious Concerns" # If we have a reason for fish passability concern, update the original FISHPCONC call to "Serious Concerns"
-wcp$Passability[wcp$Passability %in% c("concern", "Concerns", "inadequate / unsatisfactory", "y")] <- "Some Concerns" # Create the "Some concerns" class
-wcp$Passability[wcp$Reason == "Hanging Culvert"] <- "Serious Concerns" # If culvert is hanging, classify it has Serious Concerns
-wcp <- wcp[!is.na(wcp$Passability), ] # Remove culverts with NA passability calls, null, or unknown
-wcp <- wcp[wcp$Passability %in% c("No Concerns", "Some Concerns", "Serious Concerns"), ] 
-wcp$Reason[wcp$Reason == "NA"] <- NA
-wcp <- wcp[, c("FID", "INSP_DATE", "POINT_Y", "POINT_X", "Passability", "Reason")] # Filter survey columns
-colnames(wcp) <- c("SurveyID", "SurveyDate", "Latitude", "Longitude", "Passability", "PassabilityConcern")
+wcp <- wcp[-grep("ABMI", wcp$REMARKS), ] # Remove the duplicated ABMI surveys performed by Woodlands North
+wcp <- wcp[!is.na(wcp$STR_CLASS), ] # Remove culverts with no stream information
+wcp <- wcp[!(wcp$STR_CLASS %in% c("null", "Unknown")), ]
+wcp <- wcp[wcp$FISHPCONC %in% c("Concerns", "No Concerns"), ] # Extract only surveys with fish passability
+wcp$Passability <- wcp$FISHPCONC
+wcp$PassabilityConcern <- wcp$FISHPCONC
+wcp$PassabilityConcern[wcp$CV_OUT_TY %in% "Hanging"] <- "Hanging Culvert" # Define fish passability as culvert if culvert outlet is hanging
+wcp$CV_OTGAP[is.na(wcp$CV_OTGAP)] <- 0 # If no outlet hap score, assume 0
+wcp$CV_OTGAP[wcp$CV_OTGAP == "null"] <- 0
+wcp$CV_OTGAP[wcp$CV_OTGAP == "0.10M"] <- 0.10 # Type
+wcp$CV_OTGAP <- as.numeric(wcp$CV_OTGAP)
+wcp$PassabilityConcern[wcp$PassabilityConcern %in% "Hanging Culvert" & wcp$CV_OTGAP < 10] <- "Concerns" # If hang is less than 10cm, classify as normal blockage 
+wcp$BLOCKAGE[is.na(wcp$BLOCKAGE)] <- "No" # General blockages
+wcp$PassabilityConcern[wcp$PassabilityConcern %in% "Hanging Culvert" & wcp$BLOCKAGE %in% c("Potential", "Yes")] <- "Concerns" 
+wcp$BLK_BEAVR[is.na(wcp$BLK_BEAVR)] <- "No" # Beaver blockages
+wcp$PassabilityConcern[wcp$PassabilityConcern %in% "Hanging Culvert" & !(wcp$BLK_BEAVR %in% c("no", "No", "null"))] <- "Concerns" 
+wcp$BLK_TRASH[is.na(wcp$BLK_TRASH)] <- "No" # Trash blockages
+wcp$PassabilityConcern[wcp$PassabilityConcern %in% "Hanging Culvert" & !(wcp$BLK_TRASH %in% c("no", "No", "null"))] <- "Concerns"
+wcp$Project <- "WCP"
+wcp <- wcp[, c("FID", "INSP_DATE", "Project", "POINT_Y", "POINT_X", "STR_CLASS", "Passability", "PassabilityConcern")] # Filter survey columns
+colnames(wcp) <- c("SurveyID", "SurveyDate", "Project", "Latitude", "Longitude", "StreamClass", "Passability", "PassabilityConcern")
 wcp <- wcp[!duplicated(wcp[, -1]), ] # Remove duplicated calls
 
 # Stitch the data together
@@ -77,7 +91,7 @@ culvert.data <- rbind.data.frame(woodlands.north, national.parks)
 culvert.data <- rbind.data.frame(culvert.data, wcp)
 
 # Save as R object and spatial file
-comment(culvert.data) <- "Culvert data was cleaned and filtered on April 5th, 2023"
+comment(culvert.data) <- "Culvert data was cleaned and filtered on May 18th, 2023"
 save(culvert.data, file = "data/processed/culverts/culvert-surveys-cleaned.Rdata")
 rm(list=ls())
 gc()
@@ -136,7 +150,7 @@ for (location.id in 1:nrow(duplicate.list)) {
         # In case there were multiple values
         duplicate.point <- duplicate.point[1, ] # Pick first record
 
-        # Remove duplicates from base geometry
+        # Remove duplicates
         culvert.data <- culvert.data[!(culvert.data$SurveyID %in% duplicate.point$SurveyID), ]
         
 }
@@ -181,7 +195,7 @@ for(HUC in watershed.ids) {
         arcpy$SpatialJoin_analysis(target_features = "Culverts", 
                                    join_features = paste0(getwd(), "/data/processed/huc-", huc.scale, "/", 
                                                           hfi.year, "/gis/survey_temp.shp"), 
-                                   join_operation = "JOIN_ONE_TO_ONE", 
+                                   join_operation = "JOIN_ONE_TO_MANY", 
                                    join_type = "KEEP_COMMON",
                                    match_option = "WITHIN_A_DISTANCE_GEODESIC",
                                    search_radius = "50 Meters",
@@ -208,8 +222,30 @@ for(HUC in watershed.ids) {
         }
         
         matching.culverts$Node <- paste0(matching.culverts$StreamID_1, "-", matching.culverts$StreamID)
-        matching.culverts <- matching.culverts[, c("Node", "SurvyDt", "Pssblty", "PssbltC")]
-        colnames(matching.culverts) <- c("Node", "SurveyDate", "Passability", "PassabilityConcern")
+        matching.culverts <- matching.culverts[, c("Node", "SurvyDt", "StrmCls", "Pssblty", "PssbltC")]
+        colnames(matching.culverts) <- c("Node", "SurveyDate", "StreamClass", "Passability", "PassabilityConcern")
+        
+        # There will be some culverts with multiple potential surveys. Take the most recent matching one
+        duplicated.surveys <- unique(matching.culverts$Node[duplicated(matching.culverts$Node)])
+        
+        for(survey in duplicated.surveys) {
+                
+                # Store copy of surveys
+                copy.surveys <- matching.culverts[matching.culverts$Node == survey, ]
+                
+                # Remove from main file
+                matching.culverts <- matching.culverts[matching.culverts$Node != survey, ]
+                
+                # Isolate most recent survey. If multiple, pick the first one
+                copy.surveys <- copy.surveys[copy.surveys$SurveyDate == max(copy.surveys$SurveyDate), ]
+                copy.surveys <- copy.surveys[1, ]
+                
+                # Rebind to the data
+                matching.culverts <- rbind(matching.culverts, copy.surveys)
+                
+        }
+        
+        # Merge with the original watershed data
         watershed.network$Edge_Cleaned <- merge.data.frame(watershed.network$Edge_Cleaned, 
                                                            matching.culverts, by = "Node", all = TRUE)
         
@@ -234,5 +270,5 @@ for(HUC in watershed.ids) {
         
 }
 
-comment(culvert.attributes) <- "Culvert data was matched with GIS attributes on April 24th, 2023"
+comment(culvert.attributes) <- "Culvert data was matched with GIS attributes on May 18th, 2023"
 save(culvert.attributes, file = "data/processed/culverts/culvert-model-attributes.Rdata")
