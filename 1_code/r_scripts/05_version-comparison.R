776a091e47d7c01a370e2424683d4dd3feaabeee
Brandon Allen <ballen@ualberta.ca>
2026-05-15 09:51:39 -0600

    Correct visualization of differences

--- 1_code/r_scripts/05_version-comparison.R
+++ 1_code/r_scripts/05_version-comparison.R
@@ -414,7 +414,7 @@ updated.2023$Difference <- updated.2023$Connect - updated.2010$Connect
 
 connect.diff <- ggplot() + 
         geom_sf(data = updated.2023, aes(color = Difference, fill = Difference), show.legend = TRUE) +
-        scale_fill_gradientn(name = expression(Delta*Connectivity), colors = c("#e66101", "#fdb863", "#f7f7f7", "#b2abd2", "#5e3c99"), limits = c(-25,25), guide = "colourbar") +
+        scale_fill_gradientn(name = expression(Delta*Connectivity), colors = c("#e66101", "#fdb863", "#f7f7f7"), limits = c(-25,0), guide = "colourbar") +
         scale_color_gradientn(colors = "#000000", guide = "none") +
         theme_light() +
         theme(axis.title.x = element_blank(),
